2a4f5edc5442841e6a6538d25b3d2115d752a964
eun-n <eunicena.12@gmail.com>
2016-06-06 17:17:08 -0700

    update

--- script.js
+++ script.js
@@ -2,16 +2,30 @@
 function eye () {
 	document.querySelector("#coaster").src = 'assets/taeyeon1.mp4';	
 	document.querySelector("#currentvid").load();
+	play1();
 	toggle();
 }
 
 function rain () {
 	document.querySelector("#coaster").src = 'assets/taeyeon.mp4';
 	document.querySelector("#currentvid").load();
+	play1();
 	toggle();
 }
 
 function toggle() {
 	document.getElementById('test').setAttribute('visible', !document.getElementById('test').getAttribute('visible'));
 	document.getElementById('test1').setAttribute('visible', !document.getElementById('test1').getAttribute('visible'));
+}
+
+function play1() {
+	document.getElementById('coaster').play();
+	document.getElementById('play').setAttribute('visible', false);
+	document.getElementById('pause').setAttribute('visible', true);
+}
+
+function pause1() {
+	document.getElementById('coaster').pause();
+	document.getElementById('play').setAttribute('visible', true);
+	document.getElementById('pause').setAttribute('visible', false);
 }
