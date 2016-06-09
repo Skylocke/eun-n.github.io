3a76d587755de183d54cdba8bebef821911402f5
eun-n <eunicena.12@gmail.com>
2016-06-09 16:12:21 -0700

    attempted to add local storage for video playtime

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var arr = ['assets/exo.mp4', 'assets/taeyeon.mp4', 'assets/FantasticBaby.mp4', 'assets/ptdemo1.mp4', 'assets/ptdemo2.mp4', 'assets/ptdemo3.mp4'];
 var aa = 3;
 
+
 function vid (a) {
 	document.querySelector("#coaster").src = arr[a];	
 	document.querySelector("#currentvid").load();
